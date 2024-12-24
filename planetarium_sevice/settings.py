7624cfa97e9f637ab29d422011cf352f75cb2c09
Соня Cавкова <silva.vvss12@gmail.com>
2024-12-24 12:11:32 +0200

    added app user

--- planetarium_sevice/settings.py
+++ planetarium_sevice/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'planetarium'
+    'planetarium',
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
